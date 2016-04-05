1dc643368b8b796e540d9359ef7a169d514d4414
Yutaka <yutaka@icoro.com>
2016-04-06 03:08:03 +0900

    ほかに移動、もしくは使われていないコードを削除

--- include/class_view.php
+++ include/class_view.php
@@ -8,9 +8,6 @@ class SimpleAmazonView {
 	private $options;
 	private $styles;
 	private $lib;
-//	private $domain;
-//	private $tld;
-//	private $img;
 
 	/**
 	 * @param	none
@@ -23,27 +20,6 @@ class SimpleAmazonView {
 		$this->options = $simple_amazon_options;
 		$this->lib = new SimpleAmazonLib();
 
-		// デフォルトのドメインを設定
-/*
-		switch( WPLANG ) {
-			case 'en_CA': $this->domain = 'amazon.ca'; $this->tld = 'ca'; break;
-			case 'zh_CN': $this->domain = 'amazon.cn'; $this->tld = 'cn'; break;
-			case 'de_DE': $this->domain = 'amazon.de'; $this->tld = 'de'; break;
-			case 'es_ES': $this->domain = 'amazon.es'; $this->tld = 'es'; break;
-			case 'fr_FR': $this->domain = 'amazon.fr'; $this->tld = 'fr'; break;
-			case 'it_IT': $this->domain = 'amazon.it'; $this->tld = 'it'; break;
-			case 'ja':    $this->domain = 'amazon.co.jp'; $this->tld = 'jp'; break;
-			case 'en_GB': $this->domain = 'amazon.co.uk'; $this->tld = 'uk'; break;
-			default:      $this->domain = 'amazon.com'; $this->tld = 'com';
-		}
-*/
-/*
-		$this->img = array(
-			'small'     => SIMPLE_AMAZON_IMG_URL . '/amazon_noimg_small.png',
-			'medium'    => SIMPLE_AMAZON_IMG_URL . '/amazon_noimg.png',
-			'large'     => SIMPLE_AMAZON_IMG_URL . '/amazon_noimg_large.png'
-		);
-*/
   	}
  
 
@@ -55,13 +31,7 @@ class SimpleAmazonView {
 	 * @return	none
 	 */
 	public function view( $asin, $code, $styles ) {
-/*
-		if($code) {
-			// set TLD
-			$domain = $this->lib->get_domain($code);
-			$this->tld = $this->lib->get_TLD($this->domain);
-		}
-*/
+
 		$domain = $this->lib->get_domain($code);
 
 		$display = $this->generate( $asin, $domain, $styles );
@@ -258,9 +228,6 @@ class SimpleAmazonView {
 		$attr = $item->ItemAttributes;
 		$url  = $item->DetailPageURL;
 
-//		if( $layout_type != 3 )
-//			$img = $this->lib->get_img($item, $imgsize);
-
 		// テンプレート //
 		
 		//image
@@ -398,179 +365,7 @@ class SimpleAmazonView {
 		return $output;
 
 	}
-	
-
-	/**
-	 * 国コードからドメインを取得する
-	 * @param String $code
-	 * @return String $domain
-	 */
-/*
-	private function get_domain( $code ) {
-		switch($code) {
-			case 'ca':        $domain = 'amazon.ca'; break;
-			case 'cn':        $domain = 'amazon.cn'; break;
-			case 'de':        $domain = 'amazon.de'; break;
-			case 'es':        $domain = 'amazon.es'; break;
-			case 'fr':        $domain = 'amazon.fr'; break;
-			case 'it':        $domain = 'amazon.it'; break;
-			case 'jp':        $domain = 'amazon.co.jp'; break;
-			case 'uk':        $domain = 'amazon.co.uk'; break;
-			case 'javari.jp': $domain = 'javari.jp'; break;
-			case 'us':        $domain = 'amazon.com'; break;
-			default:          $domain = $this->domain;
-		}
-		return $domain;
-	}
-*/
-
-	/**
-	 * ドメインからTLDを取得する
-	 * @param String $domain
-	 * @return String $tld
-	 */
-/*
-	private function get_TLD( $domain ) {
-		switch($domain) {
-			case 'amazon.ca':    $tld = 'ca'; break;
-			case 'amazon.cn':    $tld = 'cn'; break;
-			case 'amazon.de':    $tld = 'de'; break;
-			case 'amazon.es':    $tld = 'es'; break;
-			case 'amazon.fr':    $tld = 'fr'; break;
-			case 'amazon.it':    $tld = 'it'; break;
-			case 'amazon.co.jp': $tld = 'jp'; break;
-			case 'amazon.co.uk': $tld = 'uk'; break;
-			case 'javari.jp':    $tld = 'jp'; break;
-			case 'amazon.com':   $tld = 'com'; break;
-			default:             $tld = $this->tld;
-		}
-		return $tld;
-	}
-*/
-
-	/**
-	 * TLDからアソシエイトIDを取得する
-	 * @param String $tld
-	 * @return String $aid
-	 */
-/*
-	private function get_aid($tld) {
-		
-		switch($tld) {
-			case 'ca': $aid = $this->options['associatesid_ca']; break;
-			case 'cn': $aid = $this->options['associatesid_cn']; break;
-			case 'de': $aid = $this->options['associatesid_de']; break;
-			case 'es': $aid = $this->options['associatesid_es']; break;
-			case 'fr': $aid = $this->options['associatesid_fr']; break;
-			case 'it': $aid = $this->options['associatesid_it']; break;
-			case 'jp': $aid = $this->options['associatesid_jp']; break;
-			case 'uk': $aid = $this->options['associatesid_uk']; break;
-			case 'com': $aid = $this->options['associatesid_us']; break;
-			default: $aid = '';
-		}
-
-		return $aid;
-	}
-*/
-
-	/**
-	 * 画像のURL、width、heightを設定する
-	 * @param Object $xml
-	 * @param String $imgsize
-	 * @return Object $img
-	 */
-/*
-	private function get_img( $xml, $imgsize ) {
-
-		$img = new stdClass();
-
-		switch( $imgsize ) {
-			case 'small':
-				if( property_exists($xml, 'SmallImage') ){
-					$img = $xml->SmallImage;
-				} else {
-					$img->URL    = $this->img['small'];
-					$img->Width  = 75;
-					$img->Height = 75;
-				}
-				break;
-			case 'large':
-				if( property_exists($xml, 'LargeImage') ){
-					$img = $xml->LargeImage;
-				} else {
-					$img->URL    = $this->img['large'];
-					$img->Width  = 500;
-					$img->Height = 500;
-				}
-				break;
-			default:
-				if( property_exists($xml, 'MediumImage') ){
-					$img = $xml->MediumImage;
-				} else {
-					$img->URL    = $this->img['medium'];
-					$img->Width  = 160;
-					$img->Height = 160;
-				}
-		}
-
-//		var_dump($img);
-
-		return $img;
-
-	}
-*/
-
 
 }
 
-/******************************************************************************
- * ISBN13とISBN10を相互に変換するクラス
- *****************************************************************************/
-/*
-class CalcISBNLibrary {
-
-	public function calc_chkdgt_mod10( $val ){
-		$f = 0;
-		$g = 0;
-		$k = 0;
-
-		$mod_res = explode( ',',chunk_split( $val, 1, ',' ) );
-		for( $ii=count( $mod_res )-1; $ii>-1; $ii-- ){
-			$x=intval( $mod_res[$ii] );
-			if( $f == 0 ){
-				$k += $x;
-				$f = 1;
-			} else {
-				$g += $x;
-				$f = 0;
-			}
-		}
-		$chkdgt = substr( strval( 10-intval( substr( strval( $g*3 + $k ), -1 ) ) ),-1 );
-		return $val.$chkdgt;
-	}
-
-	public function calc_chkdgt_isbn10( $val ){
-		$g = 0;
-
-		$mod_res = explode( ',',chunk_split( $val, 1, ',' ) );
-		for( $ii=count( $mod_res )-1; $ii>-1; $ii-- ){
-			$x=intval( $mod_res[$ii] );
-			$g += $x*( 11-( $ii+1 ));
-		}
-
-		$checksum = (( (int)( $g/11 ) )+1)*11 - $g;
-
-		if( $checksum == 11 ){
-			$chkdgt = 0;
-		} elseif( $checksum == 10 ){
-			$chkdgt = 'X';
-		} else {
-			$chkdgt = $checksum;
-		}
-
-		return $val.$chkdgt;
-	}
-
-}
-*/
 ?>
